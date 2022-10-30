b7b0476d28bfe8a00e905270d07663df8e3281cc
Den Kasyanov <den@den.ai>
2022-10-30 14:21:59 +0300

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,18 @@
+from collections import namedtuple
+
 from django.db import models
 
-# Create your models here.
+Coordinates = namedtuple("Coordinates", "latitude longitude")
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=250)
+    description_short = models.CharField(max_length=1000)
+    description_long = models.TextField()
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    @property
+    def coordinates(self):
+        return Coordinates(self.latitude, self.longitude)
