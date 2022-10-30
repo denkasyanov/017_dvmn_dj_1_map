2962b00853e33b7698689a5152e9a2591c2ecc88
Den Kasyanov <den@den.ai>
2022-10-30 17:24:10 +0300

    Serve media via development server

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,4 +23,10 @@ from .views import main_page
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", main_page),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
