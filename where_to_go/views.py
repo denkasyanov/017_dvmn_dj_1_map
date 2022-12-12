0b799e9ff286c94eb9037f352bbc7f22e91da5ad
Den Kasyanov <den@den.ai>
2022-12-12 21:15:49 +0300

    Move map features from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,34 @@
-from django.http import HttpResponse
+import json
+
 from django.shortcuts import render
+from django.templatetags.static import static
 
 
 def main_page(request):
-    return render(request, "index.html")
+    map_features = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": static("places/moscow_legends.json"),
+                },
+            },
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": static("places/roofs24.json"),
+                },
+            },
+        ],
+    }
+
+    return render(
+        request, "index.html", context={"map_features": json.dumps(map_features)}
+    )
