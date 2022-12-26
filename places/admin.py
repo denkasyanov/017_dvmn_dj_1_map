552798cfcb2dc7c765bb30c86926b4e89eb821ab
Den Kasyanov <den@den.ai>
2022-12-27 01:13:29 +0300

    Update Place admin form for easier adding/editing coordinates

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,44 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.gis.geos import Point
 
 from places.models import Image
 from places.models import Place
 
 
+class PlaceForm(forms.ModelForm):
+    """
+    This form can be used by any model that has "coordinates" field.
+    It will show a better looking map than the default one
+    """
+
+    latitude = forms.FloatField(required=False)
+    longitude = forms.FloatField(required=False)
+
+    class Meta:
+        model = Place
+        exclude = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        location = self.initial.get("location", None)
+        if isinstance(location, Point):
+            self.initial["longitude"], self.initial["latitude"] = location.tuple
+
+    def clean(self):
+        data = super().clean()
+        if "latitude" in self.changed_data or "longitude" in self.changed_data:
+            lat, lng = data.pop("latitude", None), data.pop("longitude", None)
+            data["location"] = Point(lng, lat, srid=4326)
+
+        if not (data.get("location") or data.get("latitude")):
+            raise forms.ValidationError({"location": "Location is required"})
+        return data
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    form = PlaceForm
 
 
 @admin.register(Image)
