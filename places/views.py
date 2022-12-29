02be9cb3e16d851ecb3cd06b2a68ed6f9bb9c5eb
Den Kasyanov <den@den.ai>
2022-12-30 00:49:16 +0300

    Add Place detail view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from places.models import Place
+
+
+@api_view
+def place_detail(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    return HttpResponse(place.title)
