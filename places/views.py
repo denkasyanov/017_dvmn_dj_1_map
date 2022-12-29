d01c8bd87e77163343489ed1aa8bff6608a8f6fe
Den Kasyanov <den@den.ai>
2022-12-30 01:27:54 +0300

    Return more detail about the place

--- places/views.py
+++ places/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from places.models import Place
+from places.serializers import PlaceSerializer
 
 
-@api_view
+@api_view()
 def place_detail(request, pk):
     place = get_object_or_404(Place, pk=pk)
-    return HttpResponse(place.title)
+    serializer = PlaceSerializer(place)
+    return Response(serializer.data)
