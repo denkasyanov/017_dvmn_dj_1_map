2be160c624c56801fe5e0d209a05db2f611431dc
Den Kasyanov <den@den.ai>
2022-12-30 01:27:32 +0300

    Add details to Place detail serializer

--- places/serializers.py
+++ places/serializers.py
@@ -1,7 +1,8 @@
 from django.templatetags.static import static
-from rest_framework.serializers import SerializerMethodField
+from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
+from places.models import Image
 from places.models import Place
 
 
@@ -11,7 +12,34 @@ class PlaceFeatureSerializer(GeoFeatureModelSerializer):
         geo_field = "location"
         fields = ("title", "place_id", "details_url")
 
-    details_url = SerializerMethodField()
+    details_url = serializers.SerializerMethodField()
 
     def get_details_url(self, place: Place):
         return static(f"places/{place.place_id}.json")
+
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = (
+            "title",
+            "imgs",
+            "description_short",
+            "description_long",
+            "coordinates",
+        )
+
+    imgs = serializers.SerializerMethodField()
+    coordinates = serializers.SerializerMethodField()
+
+    def get_imgs(self, place):
+        return [
+            image.image.url
+            for image in Image.objects.filter(place=place).order_by("position")
+        ]
+
+    def get_coordinates(self, place: Place):
+        return {
+            "lat": place.location.y,
+            "lng": place.location.x,
+        }
